1ed937e3512e23d86d9a832d78402c76210a9318
Ulzee An <ul1994@gmail.com>
2017-09-12 16:37:26 -0400

    refactored food interface

--- transfer_learner.py
+++ transfer_learner.py
@@ -7,15 +7,23 @@ import numpy as np
 
 import data_paths
 import cifar10, inception
-from food import food_labels, food_examples
+import food
 import tensorflow as tf
-from inception_namespace import *
 from inception_tensors import *
 
 class Transfer:
 	def __init__(self):
-		in_session = tf.Session(graph=in_graph)
+		self.in_session = tf.Session(graph=in_graph)
 
 if __name__ == '__main__':
 	# Create a TensorFlow session for executing the graph.
 	tlearner = Transfer()
+
+	# read labels on food data
+
+	labels = food.labels()
+	flattened_examples = []
+	for lbl in labels:
+		examples = food.examples(lbl, load_images=False)
+		flattened_examples += [(one, lbl.name, lbl.id) for one in examples]
+	print '1. Loaded food data:', len(flattened_examples), flattened_examples[0]
